0b3ea748535a6522cab03bbe067e5c3af3af6be0
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-02-11 21:46:08 +0500

    create(mocks, genre/artist-question-screen component)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,23 +1,54 @@
 import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import {
+  Switch,
+  Route,
+  BrowserRouter
+} from "react-router-dom";
 import PropTypes from "prop-types";
 
 const welcomeButtonHandler = () => {};
 
 const App = (props) => {
-
-  const {errorsCount} = props;
+  const {errorsCount, questions} = props;
+  const artistQuestion = questions.find((typeQuestion) =>
+    typeQuestion.type === `artist`
+  );
+  const genreQuestion = questions.find((typeQuestion) =>
+    typeQuestion.type === `genre`
+  );
 
   return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/dev-genre">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen
+            artistQuestion={artistQuestion}
+          />
+        </Route>
+        <Route exact path="/">
+          <GenreQuestionScreen
+            genreQuestion={genreQuestion}
+          />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.arrayOf(
+      PropTypes.object.isRequired
+  ),
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import questions from "./mocks/questions.js";
 
 const Settings = {
   ERROR_COUNT: 6
@@ -9,6 +10,7 @@ const Settings = {
 ReactDOM.render(
     <App
       errorsCount = {Settings.ERROR_COUNT}
+      questions = {questions}
     />,
     document.querySelector(`#root`)
 );
